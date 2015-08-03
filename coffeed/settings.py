b12d9bb65d10adad2f94f97110f94d2a86b65cbe
sekalska <sekalska89@gmail.com>
2015-08-04 00:38:42 +0200

    Modified settings.py

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = 'staticfiles'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_S3_FORCE_HTTP_URL = True
 AWS_QUERYSTRING_AUTH = False
+AWS_ACCESS_KEY_ID = os.environ.get('AWSAccessKeyId')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWSSecretKey')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWSAccessKeyId')
 
 AWS_STORAGE_BUCKET_NAME = 'myfirstpythonproject'
